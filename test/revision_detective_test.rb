bc65e5f865af246fefdedee672ad7532f0e67f2c
Avantika Agrawal <aagrawal@ampersand.seas.upenn.edu>
2010-12-16 00:35:51 -0500

    Added tests for flags and links added

--- test/revision_detective_test.rb
+++ test/revision_detective_test.rb
@@ -28,11 +28,33 @@ class MediaWikiApiTest < Test::Unit::TestCase
       Time.parse('2010-02-10T22:17:39Z'),
       "fixes, added persondata, typos fixed: august 24 \342\206\222 August 24 using [[Project:AWB|AWB]]"
     ]
+
+    @info2 = [2,
+      'Vladimir Guerrero',
+      'M',
+      '392473934',
+      '392337290',
+      'Briskbaby',
+      '+290',
+      Time.parse('2010-02-10T22:17:39Z'),
+      "fixes, added persondata, typos fixed: august 24 \342\206\222 August 24 using [[Project:AWB|AWB]]"
+    ]
   end
   
   def test_find_revision_info
     revinfo = @detective.find_revision_info(@info)
-    assert_equal([Time.parse('Sat Oct 23 20:59:04 UTC 2010'), 'SD5', "fixes, added persondata, typos fixed: august 24 \342\206\222 August 24 using [[Project:AWB|AWB]]", 6776], [revinfo[0], revinfo[1], revinfo[2], revinfo[3]])
+    assert_equal([Time.parse('Sat Oct 23 20:59:04 UTC 2010'), 'SD5', "fixes, added persondata, typos fixed: august 24 \342\206\222 August 24 using [[Project:AWB|AWB]]", 6776, 1], [revinfo[0], revinfo[1], revinfo[2], revinfo[3], revinfo[5]])
   end
 
+  def test_minor_info
+    revinfo = @detective.find_revision_info(@info2)
+    assert_equal([0], [revinfo[5]])
+  end
+
+  def test_link_info
+    revinfo = @detective.find_revision_info(@info2)
+    assert_equal([10], [revinfo[6]])
+    revinfo = @detective.find_revision_info(@info)
+    assert_equal([0], [revinfo[6]])
+  end
 end
