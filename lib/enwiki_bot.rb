51a1c2f17f84551fcf0e72fa52655a52ae51bdaa
Avantika Agrawal <aagrawal@ampersand.seas.upenn.edu>
2010-12-19 12:28:56 -0500

    Typo for process forking

--- lib/enwiki_bot.rb
+++ lib/enwiki_bot.rb
@@ -27,7 +27,7 @@ class EnWikiBot < Bot #TODO db.close
     end
     db_init
     
-    @detectives = [AuthorDetective, ExternalLinkDetective, RevisionDetective, PageDetective, ExternalLinkDetective]
+    @detectives = [RevisionDetective, AuthorDetective, ExternalLinkDetective, PageDetective]
     
     super(bot_name)
   end
@@ -38,7 +38,7 @@ class EnWikiBot < Bot #TODO db.close
       #TODO call our methods in other threads
       @detectives.each do |clazz|
         db = clazz.new(@db)
-        Proccess.fork do
+        Process.fork do
           db.investigate(info)
         end
       end
