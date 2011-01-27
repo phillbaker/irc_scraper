030aef0e9507c70ae85e9bef5fd1a246858a013d
pbaker <me@retrodict.com>
2011-01-27 01:36:16 -0500

    Addings gzip detection, not going to store that

--- lib/external_link_detective.rb
+++ lib/external_link_detective.rb
@@ -19,6 +19,7 @@ class ExternalLinkDetective < Detective
       id integer primary key autoincrement,
       revision_id integer,                              
       link text,
+      headers text,
       source text,
       description text,
       created DATE DEFAULT (datetime('now','localtime'))
@@ -34,14 +35,16 @@ SQL
     results = []
     linkarray.each do |arr|
       #puts arr.first
-      source_content_error = find_source(arr.first)
-      results << {:link => arr.first, :source => source_content_error, :description => arr.last}
+      source_content_error, headers = find_source(arr.first)
+      headers_str = Marshal.dump(headers)
+      #ignore 
+      results << {:link => arr.first, :source => headers['Content-encoding'] == 'gzip' ? 'gzip' : source_content_error, :description => arr.last, :headers => headers_str}
     end
 
     results.each do |linkentry|
       db_queue(
-        ['revision_id', 'link', 'source', 'description'],
-	      [info[2], linkentry[:link], linkentry[:source], linkentry[:description]]
+        ['revision_id', 'link', 'source', 'description', 'headers'],
+	      [info[2], linkentry[:link], linkentry[:source], linkentry[:description], linkentry[:headers]]
 	    )
     end	
     true # :)
@@ -62,15 +65,20 @@ SQL
       resp = e
     end
     
-    ret = ''
+    ret = []
     if(resp.is_a? Net::HTTPOK or resp.is_a? Net::HTTPFound)
       #truncate at 100K characters; not a good way to deal with size, should check the headers only
       #else set the body to the content type
-      ret = (resp.content_type == 'text/html') ? resp.body[0..10**5] : resp.content_type
+      if resp.content_type == 'text/html'
+        ret << resp.body[0..10**5]
+      else
+        ret << resp.content_type
+      end
     else #TODO follow redirects!
       #if it's a bad http response set the body equal to that response
-      ret = resp.class.to_s
+      ret << resp.class.to_s
     end
+    ret << resp.to_hash #the headers
     ret
   end
   
